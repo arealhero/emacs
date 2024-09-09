1b7ccb830ad37148ca7212c1b44efb7260e5e0a3
Vladislav Sharshukov <vsharshukov@gmail.com>
2024-09-09 03:29:26 +0300

    Redundant assignment was removed

--- config.el
+++ config.el
@@ -64,8 +64,7 @@
 (add-hook 'c++-mode-hook
           (lambda ()
             (setq-default standard-indent 4
-                          c-basic-offset 4)
-            (setq c-basic-offset 4)))
+                          c-basic-offset 4)))
 
 (setq-default indent-tabs-mode nil)
 
