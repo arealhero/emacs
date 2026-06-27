ea8230ae928c3acded0f7f4db90297b4256c8ed7
Vladislav Sharshukov <arealhero@macbook-air.local>
2026-06-28 00:24:27 +0300

    Added overrides for `eon` types and keywords

--- lisp/vlad/lang/eon.el
+++ lisp/vlad/lang/eon.el
@@ -10,6 +10,15 @@
              :host github
              :repo "rexim/simpc-mode"))
 
+(defun simpc-types ()
+  '("s8" "s16" "s32" "s64"
+    "u8" "u16" "u32" "u64"
+    "f32" "f64"
+    "_"))
+
+(defun simpc-keywords ()
+  '("mutable" "if" "else" "return" "while"))
+
 (define-derived-mode eon-mode simpc-mode "eon"
   "Major mode for eon programming language.")
 
